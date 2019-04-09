600631f5e76d810cfdb96a7115dca53333182678
Pavana <pavana.n.p@lxisoft.com>
2019-04-09 05:42:37 +0530

    Quiz_V_1.0

--- Quiz_V_1.0/com/bluelxians/quiz/view/QuizSessionView.java
+++ Quiz_V_1.0/com/bluelxians/quiz/view/QuizSessionView.java
@@ -3,35 +3,4 @@ import java.util.*;
 import java.io.*;
 import com.bluelxians.quiz.model.*;
 import com.bluelxians.quiz.controller.ManagerController;
-/*
-@Sarath Kumar S
-*/
-/*public class QuizSessionView
-{
-	ArrayList <Quiz> quizes = new ArrayList <Quiz> ();
-	Scanner scan=new Scanner();
-	ManagerController managerC=new ManagerController();
-	
-	public void sessionView(String session)
-	{	
-		String choice,;
-		this.managerC.readFromFile(quizes);
 
-		for(int i=0;i<quizes.size();i++)
-					{
-						if(quizes.get(i).getSession().equals(session))
-						{
-							System.out.println("i+1. "+quizes.get(i).getQuestion()+"\n"+"a)."+quizes.get(i).getOptionA()+"\n"+"b)."+quizes.get(i).getOptionB()+"\n"+"c)."+quizes.get(i).getOptionC()+"\n"+"d)."+quizes.get(i).getOptionD());
-							System.out.print("Enter your option:");
-							choice=scan.next();
-						}	
-					}
-		
-	}
-
-<<<<<<< HEAD
-}
-=======
-		}
-}*/
->>>>>>> 49745b1e933ca29dcf4c2abcb3cd6d038ec2d56a
