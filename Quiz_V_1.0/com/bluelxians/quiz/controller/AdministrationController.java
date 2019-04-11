da36b14fadba4f0d65205438f197fe15d149be64
Pavana <pavana.n.p@lxisoft.com>
2019-04-11 15:27:49 +0530

    Quiz_V_1.0

--- Quiz_V_1.0/com/bluelxians/quiz/controller/AdministrationController.java
+++ Quiz_V_1.0/com/bluelxians/quiz/controller/AdministrationController.java
@@ -3,6 +3,7 @@ import com.bluelxians.quiz.view.*;
 import com.bluelxians.quiz.model.Quiz;
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 /**
 *@author greeshma
 **/
@@ -15,14 +16,14 @@ public class AdministrationController
 	Quiz quiz;
 	
 	
-	public void loginValidation(String userName,String password)throws Exception
+	public void loginValidation(String emailId,String password)throws Exception
 	{
 		FileWriter w=new FileWriter("prop.properties",true);
 	FileReader r= new FileReader("prop.properties");
 	Properties p=new Properties();
 		p.load(r);
 		
-    	if(userName.equals(p.getProperty("managerUserName")))
+    	if(emailId.equals(p.getProperty("managerUserName")))
     	{
     		if(password.equals(p.getProperty("managerPassword")))
     		{
@@ -37,7 +38,7 @@ public class AdministrationController
 		{
 		for(int c=1;c<50;c++)
 		{
-		if(userName.equals(p.getProperty("UserName"+c)))
+		if(emailId.equals(p.getProperty("emailId"+c)))
 		{
 			if(password.equals(p.getProperty("Password"+c)))
 				{
@@ -51,23 +52,31 @@ public class AdministrationController
 		}
 	}
 }
-public void registrationDetails(String userName,String password)throws Exception
+public void registrationDetails(String emailId,String password)throws Exception
 {
 		FileWriter w=new FileWriter("prop.properties",true);
 		FileReader r= new FileReader("prop.properties");
 		BufferedReader br=new BufferedReader(r);
 		Properties p=new Properties();
-		//p.load(new FileReader("prop.properties"));
+		Pattern pattern=Pattern.compile("[a-zA-Z0-9]+@[a-z]+.[a-z]{2,3}");
+		Matcher matcher=pattern.matcher(emailId);
+		if(matcher.find()==true)
+		{
 		int count=0;
 		while(br.readLine()!=null)
 		{
 		count++;
-		System.out.println(count);
 		}
 		int c=(count-2)/4;
-		p.setProperty("UserName"+(c+1),userName);
+		p.setProperty("emailId"+(c+1),emailId);
 		p.setProperty("Password"+(c+1),password);
-		p.store(w,"java projects");	
+		p.store(w,"java projects");
+		System.out.println("registration successful");
+		}
+		else
+		{
+			System.out.println("Wrong Emailid");
+		}
 		w.close();
 }
 }
